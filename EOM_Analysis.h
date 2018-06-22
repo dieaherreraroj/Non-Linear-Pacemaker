8a9d1c99418cc8326c7093ebbf3b28059fee5b75
dieaherreraroj <dieaherreraroj@unal.edu.co>
2018-06-22 09:22:29 -0500

    Analysis Routines: Num. Integ. & DFT Analysis

---
+++ EOM_Analysis.h
@@ -0,0 +1,194 @@
+#include "EOM_Struct.h"
+
+/*******************************************************************************
+CREATOR: Diego Alejandro Herrera Rojas.
+FOR: Grupo Caos y Complejidad - National University of Colombia.
+********************************************************************************
+PROJECT: Clock Escapements as Non-linear Pacemakers.
+DESCRIPTION: Implementation of functions used in numerical solution of equations
+             of motion.
+NOTES: Compile in UNIX system, with a compiler that supports C++11 (at least).
+       Library fftw3 (last version), must be available.
+
+       Compile using g++ -std=c++11 pacemaker.cpp -lfftw3
+*******************************************************************************/
+/*******************************************************************************
+Please do not modify any of the routines below this barrier, otherwise, you may
+have bad results because of bad implementations, only access the routine above,
+or the file integration.h.
+
+P. S. If it is necessary, check with Diego Alejandro Herrera Rojas (UNAL) via
+      e-mail: dieaherreraroj@unal.edu.co
+*******************************************************************************/
+/******************************************************************************
+                     BASIC EOM DATA STRUCTURE FUNCTIONS
+******************************************************************************/
+
+void EOM_Data::initialize(int n, int N, double delta, double t_init){
+  if(0 < n && 0 < N && 0 < delta){
+    NSTEP = N;
+    dim = n;
+    dt = delta;
+    t0 = t_init;
+    init_data = (double*) calloc(dim,sizeof(double));
+    motion = (double*) calloc(dim*NSTEP,sizeof(double));
+  }
+  else
+    std::cerr << "Inappropriate initialization" << '\n';
+}
+
+void EOM_Data::kill(){
+  free(init_data);
+  free(motion);
+  free(pdf);
+}
+
+void EOM_Data::WriteCoord(int i, int j, double x){
+  if(i < NSTEP && j < dim)
+    motion[dim*i+j] = x;
+  else
+    std::cerr << "Writing Outside Array" << '\n';
+}
+
+double EOM_Data::ReadCoord(int i, int j){
+  if(i < NSTEP && j < dim)
+    return motion[dim*i+j];
+  else{
+    std::cerr << "Reading Outside Array" << '\n';
+    return 0.0;
+  }
+}
+
+void EOM_Data::print_motion(double t_begin){
+  double t = 0.0;
+  for(int ii = 0; ii < NSTEP; ii++){
+    t = t0 + ii*dt;
+    if(t > t_begin){
+      double ang = EOM_Data::ReadCoord(ii,0);
+      double theta = atan2(sin(ang),cos(ang));
+      printf("%4.7f\t %4.7e\t %4.7e\n",t,theta,EOM_Data::ReadCoord(ii,1));
+    }
+  }
+}
+
+void EOM_Data::print_spectra(double f_top){
+  double f = 0.0;
+  // Strange feature. Requires further study
+  double f_samp = 1.0/dt;
+  for(int ii = 0; f < f_top && ii < NSTEP; ii++){
+    f = (f_samp*ii)/NSTEP;
+    printf("%4.7f\t %4.7e\n",f,pdf[ii]);
+  }
+}
+
+/*******************************************************************************
+               AUXILIAR FUNCTIONS (DECLARATION & IMPLEMENTATION)
+*******************************************************************************/
+void copy_vect(int n, double *x, double *y);
+void aux_interm(int n, int tag, double h, double *x, double *y, double *z);
+/******************************************************************************/
+
+void copy_vect(int n, double *x, double *y){
+  for(int ii = 0; ii<n; ii++)
+    y[ii] = x[ii];
+}
+
+void aux_interm(int n, int tag, double h, double *x, double *y, double *z){
+  if(tag == 2 || tag == 3)
+    for(int ii = 0; ii<n; ii++)
+      z[ii] = x[ii] + 0.5*h*y[ii];
+  if(tag == 4)
+    for(int ii = 0; ii<n; ii++)
+      z[ii] = x[ii] + h*y[ii];
+}
+
+/*******************************************************************************
+             ANALISYS ROUTINES: EOM SOLVING AND FOURIER ANALISYS
+********************************************************************************
+                            EDM NUMERICAL SOLVING
+*******************************************************************************/
+
+void EOM_Struct::num_solve(){
+  // Allocate memory
+  double *y = (double*) calloc(DynSys.dim,sizeof(double));
+  double *k1 = (double*) calloc(DynSys.dim,sizeof(double));
+  double *k2 = (double*) calloc(DynSys.dim,sizeof(double));
+  double *k3 = (double*) calloc(DynSys.dim,sizeof(double));
+  double *k4 = (double*) calloc(DynSys.dim,sizeof(double));
+  double *aux = (double*) calloc(DynSys.dim,sizeof(double));
+  double t = DynSys.t0;
+  // Set initial conditions
+  copy_vect(DynSys.dim,DynSys.init_data,y);
+  // Core of routine: Integration steps
+  for(int ii = 0; ii < DynSys.NSTEP; ii++){
+    t = DynSys.t0 + ii*DynSys.dt;
+    // Update numerical solution data in EOM_Data Structure
+    DynSys.EOM_Data::WriteCoord(ii,0,atan2(sin(y[0]),cos(y[0])));
+    for(int jj = 1; jj < DynSys.dim; jj++)
+      DynSys.EOM_Data::WriteCoord(ii,jj,y[jj]);
+    // Carry out next integration step
+    // Update k1
+    for(int hh = 0; hh < DynSys.dim; hh++)
+      k1[hh] = Force.EOM_Force::force(hh,t,y);
+    // Update k2
+    aux_interm(DynSys.dim,2,DynSys.dt,y,k1,aux);
+    for(int hh = 0; hh < DynSys.dim; hh++)
+      k2[hh] = Force.EOM_Force::force(hh,t+0.5*DynSys.dt,aux);
+    // Update k3
+    aux_interm(DynSys.dim,3,DynSys.dt,y,k2,aux);
+    for(int hh = 0; hh < DynSys.dim; hh++)
+      k3[hh] = Force.EOM_Force::force(hh,t+0.5*DynSys.dt,aux);
+    // Update k4
+    aux_interm(DynSys.dim,4,DynSys.dt,y,k3,aux);
+    for(int hh = 0; hh < DynSys.dim; hh++)
+      k4[hh] = Force.EOM_Force::force(hh,t+DynSys.dt,aux);
+    // Fill array with data generated
+    for(int jj = 0; jj < DynSys.dim; jj++)
+      y[jj] += (1.0/6.0)*DynSys.dt*(k1[jj] + k4[jj] + 2.0*(k2[jj] + k3[jj]));
+  }
+  // Release memory
+  free(y);
+  free(k1);
+  free(k2);
+  free(k3);
+  free(k4);
+  free(aux);
+}
+
+/*******************************************************************************
+                      STEADY STATE MOTION FOURIER ANALISYS
+*******************************************************************************/
+
+void EOM_Struct::dft_spectra(double t_min){
+  // Variables for Fourier Analysis
+  fftw_complex *in, *out;
+  fftw_plan plan;
+  // Allocate memory
+  DynSys.pdf = (double*) calloc(DynSys.NSTEP, sizeof(double));
+  in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)*DynSys.NSTEP);
+  out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)*DynSys.NSTEP);
+  // Create plan
+  plan = fftw_plan_dft_1d(DynSys.NSTEP,in,out,FFTW_FORWARD,FFTW_ESTIMATE);
+  // Test Filling: Works ok. Problem is in array filling.
+  double t = 0.0;
+  for(int ii = 0; ii < DynSys.NSTEP; ii++){
+    t = DynSys.t0 + ii*DynSys.dt;
+    /*
+    in[ii][0] = cos(Force.w*t);
+    in[ii][1] = 0.0;*/
+
+    if(t >= t_min)
+      in[ii][0] = DynSys.EOM_Data::ReadCoord(ii,0);
+    else
+      in[ii][0] = 0.0;
+    in[ii][1] = 0.0;
+  }
+  // Compute DFT
+  fftw_execute(plan);
+  double p = 0.0;
+  // Fill dft array
+  for(int ii = 0; ii < DynSys.NSTEP; ii++){
+    p = out[ii][0]*out[ii][0] + out[ii][1]*out[ii][1];
+    DynSys.pdf[ii] = DynSys.dt*DynSys.dt*p;
+  }
+}
---
+++ EOM_Struct.h
@@ -0,0 +1,104 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <fftw3.h>
+
+/*******************************************************************************
+Please, just modify parameters of structure EOM_Force if you need to solve a
+different equation of motion, with different parameters. Go to end of file if
+you want to change the Force.
+
+P. S. Mantaining the EDM_Struct and EOM_Data struct is essencial for the good
+      behavior of the programm. Under no circumstances should you modify these
+      structures.
+
+In this file you have acces to all declarations of functions of importance. They
+tell you wich are the parameters of each one.
+*******************************************************************************/
+/*******************************************************************************
+                             AUXILIAR DEFINITIONS
+*******************************************************************************/
+
+using fptr = double(int, double, double *);
+
+/*******************************************************************************
+                       BASIC EOM DATA STORING STRUCTURE
+*******************************************************************************/
+
+struct EOM_Data{
+
+  int dim;
+  int NSTEP;
+  double dt;
+  double t0;
+  double *init_data;
+  double *motion;
+  double *pdf;
+
+  EOM_Data() {
+    dim = 2;
+    NSTEP = 1000;
+    dt = 0.01;
+    t0 = 0.0;
+  };
+
+  void initialize(int n, int N, double delta, double t_init);
+  void kill();
+  void WriteCoord(int i, int j, double x);
+  double ReadCoord(int i, int j);
+  void print_motion(double t_begin);
+  void print_spectra(double f_top);
+  void rk4_integration(fptr f);
+
+};
+
+/*******************************************************************************
+                        STRUCTURES FOR EOM SOLVING
+********************************************************************************
+This is the only part you should modify, in here you can define all parameters
+for almos every external force you can imagine. Please do not try to change any
+other part of the file. Define the parameters inside EOM_Force as desired and
+remember to modify memeber function force() accordingly.
+*******************************************************************************/
+
+struct EOM_Force{
+  double F;
+  double w;
+  double q;
+
+  EOM_Force() {
+    F = 0.0;
+    w = 2.0*M_PI;
+    q = 0.0;
+  };
+
+  double force(int comp, double t, double *y);
+};
+
+/******************************************************************************/
+
+struct EOM_Struct{
+  EOM_Data DynSys;
+  EOM_Force Force;
+
+  void num_solve();
+  void dft_spectra(double t_min);
+};
+
+/*******************************************************************************
+                      DEFINITION OF EXTERNAL FORCES
+********************************************************************************
+This you can modify as much as you desire, this function contains information of
+external forces on the system. For sake of consistency, you can modify force pa-
+rameters by editing EOM_Force structure.
+*******************************************************************************/
+
+double EOM_Force::force(int comp, double t, double *y){
+  if(comp == 1) return -w*w*sin(y[0])-q*y[1]+F;
+    else if (comp == 0) return y[1];
+  else{
+    std::cerr << "No more dependent variables" << '\n';
+    return 0.0;
+  }
+}
